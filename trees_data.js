a2c525247778077c9677d86861b01d60f7c61828
Theo Lee <theolee021@gmail.com>
2024-10-29 14:23:27 -0700

    Create Tree Map

---
+++ trees_data.js
@@ -0,0 +1,6 @@
+const trees = [
+    { species: 'American Sycamore', height: 18.75, latitude: 37.3204977, longitude: -122.0459386 },
+    { species: 'Maple', height: 10, latitude: 37.7849, longitude: -122.4094 },
+    { species: 'Pine', height: 20, latitude: 37.7649, longitude: -122.4294 }
+  ];
+  
